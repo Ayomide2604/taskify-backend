1d237b8cfc034399a961210689c8f6969ca6c668
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-04-30 22:45:39 +0100

    Added Task serializer, viewset and url configuration

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name' 'last_name', 'username']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
 
     def __str__(self) -> str:
         return self.email
--- task/views.py
+++ task/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Task
+from .serializers import TaskSerializer
 # Create your views here.
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.order_by('created_at').all()
+    serializer_class = TaskSerializer

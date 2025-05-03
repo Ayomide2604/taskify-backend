4c87f46199bfab2ec7ac718786e9fec654ef7af1
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-05-03 21:49:16 +0100

    Updated allowed host to backend url on railway

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['railway.app']
+ALLOWED_HOSTS = ['web-production-d2111.up.railway.app']
 
 
 # Application definition
